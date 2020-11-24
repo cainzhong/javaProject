b5b8c826adfd4e4411366b74244e0f6082341e22
Susie Hu <si-qinh@microfocus.com>
2020-11-24 19:18:28 +0800

    import countDownLatch

--- Basic/src/test/java/org/example/ParallelStreamTest.java
+++ Basic/src/test/java/org/example/ParallelStreamTest.java
@@ -13,60 +13,72 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.OptionalInt;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TransferQueue;
+import java.util.concurrent.*;
 import java.util.stream.IntStream;
+
 import org.junit.Test;
 
 /**
- *@ClassName ParallelStreamTest
- *@Description TODO
- *@Date 11/20/2020 11:14 AM
- *@Version 1.0
+ * @ClassName ParallelStreamTest
+ * @Description TODO
+ * @Date 11/20/2020 11:14 AM
+ * @Version 1.0
  **/
 public class ParallelStreamTest {
 
-  class Item {
-    private  Integer id;
+    class Item {
+        private Integer id;
 
-    public void setId(Integer id) {
-      this.id = id;
-    }
-
-    public Integer getId() {
-      return id;
-    }
-  }
-
-  @Test
-  public void test() throws InterruptedException {
-    List<Item> input = new ArrayList<>();
-    List<Integer> integers = new ArrayList<>();
-    IntStream.range(0,1000).forEach(integers::add);
-    ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(4,100,120, TimeUnit.MINUTES,new LinkedBlockingDeque<>());
-
-    IntStream.range(0,1000).forEach(i -> {
-      Optional<Item> optional = input.parallelStream().filter(item -> (item.getId().equals(i))).findAny();
-      threadPoolExecutor.submit(new Runnable() {
-        @Override
-        public void run() {
-          assertTrue(optional.get().getId().equals(i));
+        public void setId(Integer id) {
+            this.id = id;
         }
-      });
 
-    });
+        public Integer getId() {
+            return id;
+        }
+    }
 
-    while(true){
-      if(threadPoolExecutor.getActiveCount() == 0){
+    @Test
+    public void test() {
+        List<Item> input = new ArrayList<>();
+        List<Integer> integers = new ArrayList<>();
+        int count = 10000;
+        CountDownLatch countDownLatch = new CountDownLatch(count);
+        IntStream.range(0, count).forEach(integers::add);
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(4, 100, 120, TimeUnit.MINUTES, new LinkedBlockingDeque<>());
+
+        IntStream.range(0, count).forEach(i -> {
+            Optional<Item> optional = input.parallelStream().filter(item -> (item.getId().equals(i))).findAny();
+            threadPoolExecutor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        assertTrue(optional.get().getId().equals(i));
+                        System.out.println(i);
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } finally {
+                        countDownLatch.countDown();
+                    }
+                }
+            });
+
+        });
+
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println("All done");
         threadPoolExecutor.shutdown();
-        break;
-      }
-      Thread.sleep(1000);
-      System.out.print("...");
     }
-  }
 
+    @Test
+    public void test_retry(){
+        for (int i = 0; i < 10; i++) {
+            test();
+        }
+    }
 }
